f25d5b3cef36933cda02e927563c56bd8fda84aa
navatharshini <107050309+navatharshini@users.noreply.github.com>
2024-03-17 14:07:42 +0530

    Create week01TakeHome03.cpp

---
+++ week01TakeHome03.cpp
@@ -0,0 +1,17 @@
+//Print the sum of the three numbers on a single line
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int a,b,c;
+    cin >> a >> b >>c;   //take three elements
+    cout << a+b+c;  // add that three elements
+    return 0;
+}
